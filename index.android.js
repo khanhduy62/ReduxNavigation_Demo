dc47e98393dc48fa63ef81385f2b783d98f154ba
lekhanhduy <lekhanhduyduy1201@gmail.com>
2017-06-26 14:40:27 +0700

    first commit

---
+++ index.android.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+
+ import React,  { Component } from 'react';
+ import { AppRegistry } from 'react-native';
+ import { Provider } from 'react-redux';
+ import { createStore } from 'redux';
+
+ import AppReducer from './src/reducers';
+ import AppWithNavigationState from './src/navigators/AppNavigator';
+
+export default class ReduxNavigation_Demo extends Component {
+  store = createStore(AppReducer);
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
+  }
+}
+
+
+
+AppRegistry.registerComponent('ReduxNavigation_Demo', () => ReduxNavigation_Demo);
